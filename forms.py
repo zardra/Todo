057f5acf3addd3a3f517c207df1542174cd0bfda
Liz Borchardt <zardra@gmail.com>
2014-02-07 14:01:21 -0800

    setting up login

---
+++ forms.py
@@ -0,0 +1,9 @@
+from wtforms import Form, TextField, TextAreaField, PasswordField, validators
+
+class LoginForm(Form):
+    username = TextField("Username", [validators.Required()])
+    password = PasswordField("Password", [validators.Required()])
+
+class NewPostForm(Form):
+    title = TextField("title", [validators.Required()])
+    body = TextAreaField("body", [validators.Required()])
